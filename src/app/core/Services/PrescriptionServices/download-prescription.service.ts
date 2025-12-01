3cd2d721d59c2fc079c3df8327294f20a9b65bed
Adishree <adishree@argusoft.com>
2025-12-01 09:52:40 +0530

    download prescription

---
+++ src/app/core/Services/PrescriptionServices/download-prescription.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DownloadPrescriptionService {
+
+  private apiUrl = 'http://localhost:8089/patient/prescriptions/download'; // backend URL
+
+  constructor(private http: HttpClient) {}
+
+  downloadSuperPrescriptionPdf(superPrescriptionId: number) {
+    return this.http.get(`${this.apiUrl}/${superPrescriptionId}/pdf`, {
+      responseType: 'blob' 
+    });
+  }
+}
